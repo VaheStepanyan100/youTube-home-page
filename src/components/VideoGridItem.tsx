b023f57bc549c2e14060112412ce2d9d66e797cc
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-10-31 15:34:19 +0400

    Complete video item info

--- src/components/VideoGridItem.tsx
+++ src/components/VideoGridItem.tsx
@@ -1,4 +1,5 @@
-import { formatDuration } from "../utils/formatDuration";
+import { formatDuration } from '../utils/formatDuration';
+import { formatTimeAgo } from '../utils/formatTimeAgo';
 
 type VideoGridItemProps = {
   id: string;
@@ -14,6 +15,8 @@ type VideoGridItemProps = {
   thumbnailUrl: string;
   videoUrl: string;
 };
+
+const VIEW_FORMATTER = new Intl.NumberFormat(undefined, {notation: "compact"})
 
 export function VideoGridItem({
   id,
@@ -36,6 +39,22 @@ export function VideoGridItem({
           {formatDuration(duration)}
         </div>
       </a>
+      <div className="flex gap-2">
+        <a href={`/@${channel.id}`} className="flex-shrink-0">
+          <img className="w-12 h-12 rounded-full" src={channel.profileUrl} />
+        </a>
+        <div className="flex flex-col">
+          <a href={`/watch?v=${id}`} className="font-bold">
+            {title}
+          </a>
+          <a href={`/@${channel.id}`} className="text-secondary-text text-sm">
+            {channel.name}
+          </a>
+          <div className='text-secondary-text text-sm'>
+            {VIEW_FORMATTER.format(views)} Views • {formatTimeAgo(postedAt)}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
