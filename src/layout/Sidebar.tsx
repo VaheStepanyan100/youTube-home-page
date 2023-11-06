d5f4dc7d14bea088c128e7de8bc98146cdc5d533
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-11-06 22:57:34 +0400

    Create LargeSidebarSection

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,16 +1,35 @@
-import { Clapperboard, Home, Library, Repeat } from 'lucide-react';
-import { ElementType } from 'react';
-import { buttonStyles } from '../components/Button';
+import {
+  ChevronDown,
+  ChevronUp,
+  Clapperboard,
+  Home,
+  Library,
+  Repeat,
+} from 'lucide-react';
+import { Children, ElementType, ReactNode, useState } from 'react';
+import Button, { buttonStyles } from '../components/Button';
 import { twMerge } from 'tailwind-merge';
 
 export function Sidebar() {
   return (
-    <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
-      <SmallSidebarItem Icon={Home} title="Home" url="/" />
-      <SmallSidebarItem Icon={Repeat} title="Shorts" url="/shorts" />
-      <SmallSidebarItem Icon={Clapperboard} title="Subscriptions" url="/subsriptions" />
-      <SmallSidebarItem Icon={Library} title="Library" url="/library" />
-    </aside>
+    <>
+      <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
+        <SmallSidebarItem Icon={Home} title="Home" url="/" />
+        <SmallSidebarItem Icon={Repeat} title="Shorts" url="/shorts" />
+        <SmallSidebarItem
+          Icon={Clapperboard}
+          title="Subscriptions"
+          url="/subsriptions"
+        />
+        <SmallSidebarItem Icon={Library} title="Library" url="/library" />
+      </aside>
+      <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 flex">
+        <LargeSidebarSection visibleItemCount={1}>
+          <LargeSidebarItem isActive Icon={Home} title="Home" url="/" />
+          <LargeSidebarItem Icon={Home} title="Home" url="/" />
+        </LargeSidebarSection>
+      </aside>
+    </>
   );
 }
 
@@ -22,9 +41,84 @@ type SmallSidebarItemProps = {
 
 function SmallSidebarItem({ Icon, title, url }: SmallSidebarItemProps) {
   return (
-    <a href={url} className={twMerge(buttonStyles({ variant: 'ghost' }), "py-4 px-1 flex flex-col items-center rounded-lg gap-1")}>
+    <a
+      href={url}
+      className={twMerge(
+        buttonStyles({ variant: 'ghost' }),
+        'py-4 px-1 flex flex-col items-center rounded-lg gap-1'
+      )}
+    >
       <Icon className="w-6 h-6" />
       <div className="text-sm">{title}</div>
+    </a>
+  );
+}
+
+type LargeSidebarSectionProps = {
+  children: ReactNode;
+  title?: string;
+  visibleItemCount?: number;
+};
+
+function LargeSidebarSection({
+  children,
+  title,
+  visibleItemCount = Number.POSITIVE_INFINITY,
+}: LargeSidebarSectionProps) {
+  const [isExpended, setIsExpended] = useState(false);
+  const childrenArray = Children.toArray(children).flat();
+  const showExpendButton = childrenArray.length > visibleItemCount;
+  const visibleChildren = isExpended
+    ? childrenArray
+    : childrenArray.slice(0, visibleItemCount);
+  const ButtonIcon = isExpended ? ChevronUp : ChevronDown;
+  return (
+    <div>
+      {title && <div className="ml-4 mt-2 text-lg mb-1">{title}</div>}
+      {visibleChildren}
+      {showExpendButton && (
+        <Button
+          onClick={() => setIsExpended((e) => !e)}
+          variant="ghost"
+          className="w-full flex items-center rounded-lg gap-4 p-3"
+        >
+          <ButtonIcon className="w-6 h-6" />
+          <div>{isExpended ? 'Show Less' : 'Show More'}</div>
+        </Button>
+      )}
+    </div>
+  );
+}
+
+type LargeSidebarItemProps = {
+  Icon: ElementType;
+  title: string;
+  url: string;
+  isActive?: boolean;
+};
+
+function LargeSidebarItem({
+  Icon,
+  title,
+  url,
+  isActive = false,
+}: LargeSidebarItemProps) {
+  return (
+    <a
+      href={url}
+      className={twMerge(
+        buttonStyles({
+          variant: 'ghost',
+        }),
+        `w-full flex items-center rounded-lg gap-4 p-3 ${
+          isActive ? 'font-bold bg-neutral-100 hover:bg-secondary' : undefined
+        }`
+      )}
+    >
+      <Icon className="w-6 h-6" />
+      <div className="whitespace-nowrap overvlow-hidden text-ellipsis">
+        {title}
+      </div>
     </a>
   );
 }
