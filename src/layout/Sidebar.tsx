7b7f2e6533174d40d9b775c555f7e65ffb4bb191
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-11-08 18:45:30 +0400

    Add sidebar items

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -2,13 +2,18 @@ import {
   ChevronDown,
   ChevronUp,
   Clapperboard,
+  Clock,
+  History,
   Home,
   Library,
+  ListVideo,
+  PlaySquare,
   Repeat,
 } from 'lucide-react';
 import { Children, ElementType, ReactNode, useState } from 'react';
 import Button, { buttonStyles } from '../components/Button';
 import { twMerge } from 'tailwind-merge';
+import { playlists } from '../data/sidebar';
 
 export function Sidebar() {
   return (
@@ -24,10 +29,25 @@ export function Sidebar() {
         <SmallSidebarItem Icon={Library} title="Library" url="/library" />
       </aside>
       <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 flex">
-        <LargeSidebarSection visibleItemCount={1}>
+        <LargeSidebarSection>
           <LargeSidebarItem isActive Icon={Home} title="Home" url="/" />
-          <LargeSidebarItem Icon={Home} title="Home" url="/" />
+          <LargeSidebarItem
+            Icon={Clapperboard}
+            title="Subscriptions"
+            url="/subscriptions"
+          />
+          <hr />
         </LargeSidebarSection>
+        <LargeSidebarSection visibleItemCount={5}>
+          <LargeSidebarItem Icon={Library} title="Library" url="/library" />
+          <LargeSidebarItem Icon={History} title="History" url="/history" />
+          <LargeSidebarItem Icon={PlaySquare} title="Your Videos" url="/your-videos" />
+          <LargeSidebarItem Icon={Clock} title="Watch Later" url="/playlist?list=WL" />
+          {playlists.map(playlist => (
+            <LargeSidebarItem key={playlist.id} Icon={ListVideo} title={playlist.name} url={`/playlist=${playlist.id}`}/>
+          ))}
+        </LargeSidebarSection>
+        <hr />
       </aside>
     </>
   );
